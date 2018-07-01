03080dfa55d6994859fdc8fe6329403b2c5ea63b
Aayush Kapoor <xeoneux@icloud.com>
2018-07-01 08:17:20 +0530

    Add Bitcoin List Item

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,37 @@ class App extends React.Component {
   render() {
     return (
       <main>
-        <h1>Hello React!</h1>
+        <section className="jumbotron">
+          <h1 className="jumbotron-heading">GenB Crypto Tracker</h1>
+
+          <p className="lead text-muted">
+            Track your favourite cryptocurrencies
+          </p>
+
+          <div className="row">
+            <ul>
+              <li className="coin-item">
+                <div className="coin-image">
+                  <div className="coin-remove" />
+                  <img
+                    alt="Bitcoin"
+                    src={require(`cryptocurrency-icons/dist/svg/color/btc.svg`)}
+                  />
+                </div>
+                <div className="coin-name">
+                  <p>Bitcoin</p>
+                </div>
+                <div className="coin-info">
+                  <h4>$5364.12</h4>
+                  <p>1.0</p>
+                </div>
+                <div className="coin-rank">
+                  <p>#1</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </section>
       </main>
     );
   }
