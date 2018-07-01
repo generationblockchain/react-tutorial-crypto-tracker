599a329c2405c4c267b3b00c584aed6a8c68dedb
Aayush Kapoor <xeoneux@icloud.com>
2018-07-01 08:26:27 +0530

    List all cryptocurrencies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Data from '../data.json';
+
 class App extends React.Component {
   render() {
     return (
@@ -13,25 +15,29 @@ class App extends React.Component {
 
           <div className="row">
             <ul>
-              <li className="coin-item">
-                <div className="coin-image">
-                  <div className="coin-remove" />
-                  <img
-                    alt="Bitcoin"
-                    src={require(`cryptocurrency-icons/dist/svg/color/btc.svg`)}
-                  />
-                </div>
-                <div className="coin-name">
-                  <p>Bitcoin</p>
-                </div>
-                <div className="coin-info">
-                  <h4>$5364.12</h4>
-                  <p>1.0</p>
-                </div>
-                <div className="coin-rank">
-                  <p>#1</p>
-                </div>
-              </li>
+              {Data.map(coin => (
+                <li key={coin.id} className="coin-item">
+                  <div className="coin-image">
+                    <div className="coin-remove" />
+                    <img
+                      alt={coin.name}
+                      src={require(`cryptocurrency-icons/dist/svg/color/${
+                        coin.symbol
+                      }.svg`)}
+                    />
+                  </div>
+                  <div className="coin-name">
+                    <p>{coin.name}</p>
+                  </div>
+                  <div className="coin-info">
+                    <h4>${coin.price_usd}</h4>
+                    <p>{coin.price_btc}</p>
+                  </div>
+                  <div className="coin-rank">
+                    <p>#{coin.rank}</p>
+                  </div>
+                </li>
+              ))}
             </ul>
           </div>
         </section>
@@ -41,13 +47,17 @@ class App extends React.Component {
 
           <div className="container">
             <div className="row">
-              <div className="coin-select col-md-1">
-                <img
-                  alt="Ethereum"
-                  src={require(`cryptocurrency-icons/dist/svg/color/eth.svg`)}
-                />
-                <p>Etheruem</p>
-              </div>
+              {Data.map(coin => (
+                <div key={coin.id} className="coin-select col-md-1">
+                  <img
+                    alt={coin.name}
+                    src={require(`cryptocurrency-icons/dist/svg/color/${
+                      coin.symbol
+                    }.svg`)}
+                  />
+                  <p>{coin.name}</p>
+                </div>
+              ))}
             </div>
           </div>
         </section>
